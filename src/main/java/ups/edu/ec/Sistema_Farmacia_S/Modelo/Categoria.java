3c2e5b860c2b7db7cb2aa271c02feb3f918d3ea5
Adrian <luiscabreraucme@hotmail.com>
2022-06-29 13:40:47 -0500

    AUTO

--- src/main/java/ups/edu/ec/Sistema_Farmacia_S/Modelo/Categoria.java
+++ src/main/java/ups/edu/ec/Sistema_Farmacia_S/Modelo/Categoria.java
@@ -11,7 +11,7 @@ public class Categoria implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String nombre;
 
--- src/main/java/ups/edu/ec/Sistema_Farmacia_S/Modelo/Kardex.java
+++ src/main/java/ups/edu/ec/Sistema_Farmacia_S/Modelo/Kardex.java
@@ -8,7 +8,7 @@ public class Kardex implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     @OneToOne
     @JoinColumn
