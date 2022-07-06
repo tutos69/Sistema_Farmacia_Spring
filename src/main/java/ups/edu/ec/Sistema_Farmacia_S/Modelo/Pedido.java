60697d3aededc5178abc1ebfc869f075bf2e4bc2
MiguelSamaniego <94272369+MiguelSamaniego@users.noreply.github.com>
2022-07-06 11:49:58 -0500

    Bitacora del Capitan(NO borraran de nuevo)

--- src/main/java/ups/edu/ec/Sistema_Farmacia_S/Modelo/Pedido.java
+++ src/main/java/ups/edu/ec/Sistema_Farmacia_S/Modelo/Pedido.java
@@ -33,11 +33,17 @@ public class Pedido implements Serializable {
     @JoinColumn
     private FormaPago formaPago;
 
-    @OneToOne
-    @JoinColumn
-    private Entidad entidad;
+
+    private String identificador;
+    private String nombre;
+    private String apellido;
+    @Column(name = "correo")
+    private String correo;
+    private String direccion;
+    private String telefono;
 
     private  double total;
+
 
     public Pedido() {
     }
@@ -62,12 +68,52 @@ public class Pedido implements Serializable {
         this.total = total;
     }
 
-    public Entidad getEntidad() {
-        return entidad;
+    public String getIdentificador() {
+        return identificador;
     }
 
-    public void setEntidad(Entidad entidad) {
-        this.entidad = entidad;
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 
     public int getId() {
